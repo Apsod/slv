acd434b92f1348045fbf3ec08c1ea8ad2ccba7cc
Amaru Cuba Gyllensten <amarucuba@gmail.com>
2020-09-16 10:52:52 +0200

    ..

--- slv/kundo.py
+++ slv/kundo.py
@@ -11,65 +11,61 @@ KUNDO = 'https://kundo.se'
 def mkpath(*parts):
     return '/'.join(parts)
 
-def pagination_wrapper(request, limit=50):
-    response = request(start=0, limit=limit)
-    pages = math.ceil(response.total / limit)
-    
-    yield from response.json
-    for page in range(1, pages):
-        response = request(start=page*limit, limit=limit)
-        yield from response.json
-
-@dataclass
-class ListResponse:
-    total: int
-    json: list
-
-    @staticmethod
-    def from_response(response):
-        return ListResponse(int(response.headers['X-TotalResults']), response.json())
-
 
 class Kundo(object):
-    def __init__(self, key, include_all=False):
+    def __init__(self, key=None, include_all=False):
         self.session = requests.Session()
-        self.session.params['key'] = key
+        self.set_key(key)
         self.include_all(include_all)
+
+
+    def set_key(self, key=None):
+        if key is None:
+            self.session.params.pop('key', False)
+        else:
+            self.session.params['key'] = key
 
     def include_all(self, flag=True):
         if flag:
             self.session.params['include'] = 'all'
         else:
             self.session.params.pop('include', False)
+
+
+    def get_all(self, *path, start=0, limit=50, **params):
+        url = mkpath(KUNDO, *path)
+        response = self.session.get(url, params=dict(start=start, limit=limit, **params))
+        total = int(response.headers['X-TotalResults'])
+        pages = math.ceil(total / limit)
+        yield from response.json()
+        for page in range(1, pages):
+            response = self.session.get(url, params=dict(start=limit*page, limit=limit, **params))
+            yield from response.json()
+
     
     def get_taglist(self):
         url = mkpath(KUNDO, 'api', 'taglist', 'livsmedelsverket.json')
         return self.session.get(url).json()
 
-    def get_tagged(self, 
+
+    def get_tagged(self, tag, **params):
+        yield from self.get_all('api', 'tag', 'livsmedelsverket', '{}.json'.format(tag), **params)
+    
 
     def get_questions(self, **params):
-        url = mkpath(KUNDO, 'api', 'livsmedelsverket.json')
-        response = self.session.get(url, params=params)
-        return ListResponse.from_response(response)
+        yield from self.get_all('api', 'livsmedelsverket.json', **params)
+
 
     def get_answers(self, dialog_id, **params):
-        url = mkpath(KUNDO, 'api', 'comment', 'livsmedelsverket', '{}.json'.format(dialog_id))
-        response = self.session.get(url, params=params)
-        return ListResponse.from_response(response)
+        yield from self.get_all('api', 'comment', 'livsmedelsverket', '{}.json'.format(dialog_id), **params)
+
 
     def get_dialogs(self, **params):
         questions = self.get_questions(**params)
         pairs = []
-        for question in questions.json:
-            get_answer = functools.partial(self.get_answers, question['id'])
-            answers = list(pagination_wrapper(get_answer))
-            pairs.append(dict(
-                question=question,
-                answers=answers
-                ))
-
-        return ListResponse(questions.total, pairs)
+        for question in questions:
+            answers = list(self.get_answers(question['id']))
+            yield dict(question=question, answers=answers)
 
     def __enter__(self):
         return self
@@ -80,6 +76,6 @@ class Kundo(object):
 
 if __name__ == '__main__':
     import json
-    with Kundo(KEY) as kundo:
-        response = kundo.get_taglist()
-        print(json.dumps(response))
+    with Kundo(KEY, include_all=True) as kundo:
+        for pair in kundo.get_dialogs(tags='a catering'):
+            print(json.dumps(pair))
---
+++ slv/util/tags.py
@@ -0,0 +1,12 @@
+  
+TAGS = dict(
+        A = "a catering",
+        B = "b import vegetabilier",
+        C = "c import sammansatta",
+        D = "d kosttillskott nya livsmedel",
+        E = "e kosttillskott gravid",
+        F = "f corona buffé",
+        G = "g avsvalning och förvaring",
+        H = "h tonfisk gravid",
+        I = "i ost gravid"
+)
