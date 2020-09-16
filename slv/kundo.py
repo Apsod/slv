550b26e1037016597cebb149bdedbf805fbcfdf7
Amaru Cuba Gyllensten <amaru.cuba.gyllensten@ri.se>
2020-09-16 11:12:38 +0000

    random stuff

--- slv/kundo.py
+++ slv/kundo.py
@@ -2,6 +2,7 @@ import requests
 import math
 import os
 import functools
+import logging
 from dataclasses import dataclass
 
 KEY = os.environ.get('KUNDO')
@@ -10,6 +11,24 @@ KUNDO = 'https://kundo.se'
 
 def mkpath(*parts):
     return '/'.join(parts)
+
+def emptyGenerator():
+    if False:
+        yield None
+
+class SizedGenerator(object):
+    def __init__(self, length, f):
+        self.consumed = False
+        self.f = f
+        self.length = length
+
+    def __len__(self):
+        return self.length
+
+    def __iter__(self):
+        assert not self.consumed, "SizedGenerator can only be run once!"
+        self.consumed = True
+        return self.f()
 
 
 class Kundo(object):
@@ -34,13 +53,19 @@ class Kundo(object):
 
     def get_all(self, *path, start=0, limit=50, **params):
         url = mkpath(KUNDO, *path)
-        response = self.session.get(url, params=dict(start=start, limit=limit, **params))
-        total = int(response.headers['X-TotalResults'])
-        pages = math.ceil(total / limit)
-        yield from response.json()
-        for page in range(1, pages):
-            response = self.session.get(url, params=dict(start=limit*page, limit=limit, **params))
-            yield from response.json()
+        response0 = self.session.get(url, params=dict(start=start, limit=limit, **params))
+
+        if response0.ok:
+            total = int(response0.headers['X-TotalResults'])
+            pages = math.ceil(total / limit)
+            def g():
+                yield from response0.json()
+                for page in range(1, pages):
+                    response = self.session.get(url, params=dict(start=limit*page, limit=limit, **params))
+                    yield from response.json()
+            return SizedGenerator(total, g)
+        else:
+            return SizedGenerator(0, emptyGenerator)
 
     
     def get_taglist(self):
@@ -49,23 +74,24 @@ class Kundo(object):
 
 
     def get_tagged(self, tag, **params):
-        yield from self.get_all('api', 'tag', 'livsmedelsverket', '{}.json'.format(tag), **params)
+        return self.get_all('api', 'tag', 'livsmedelsverket', '{}.json'.format(tag), **params)
     
 
     def get_questions(self, **params):
-        yield from self.get_all('api', 'livsmedelsverket.json', **params)
+        return self.get_all('api', 'livsmedelsverket.json', **params)
 
 
     def get_answers(self, dialog_id, **params):
-        yield from self.get_all('api', 'comment', 'livsmedelsverket', '{}.json'.format(dialog_id), **params)
+        return self.get_all('api', 'comment', 'livsmedelsverket', '{}.json'.format(dialog_id), **params)
 
 
     def get_dialogs(self, **params):
         questions = self.get_questions(**params)
-        pairs = []
-        for question in questions:
-            answers = list(self.get_answers(question['id']))
-            yield dict(question=question, answers=answers)
+        def g():
+            for question in questions:
+                answers = list(self.get_answers(question['id']))
+                yield dict(question=question, answers=answers)
+        return SizedGenerator(len(questions), g)
 
     def __enter__(self):
         return self
@@ -76,6 +102,17 @@ class Kundo(object):
 
 if __name__ == '__main__':
     import json
+    import tqdm
+    from slv.util.tags import TAGS
+    def f():
+        yield 1
+        yield 2
+        yield 3
+
+    xs = SizedGenerator(3, f)
+
     with Kundo(KEY, include_all=True) as kundo:
-        for pair in kundo.get_dialogs(tags='a catering'):
-            print(json.dumps(pair))
+        with open('dump.json', 'wt') as handle:
+                for pair in tqdm.tqdm(kundo.get_dialogs()):
+                    handle.write(json.dumps(pair))
+                    handle.write('\n')
